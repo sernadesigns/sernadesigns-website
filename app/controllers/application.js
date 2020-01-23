b8c1967c48fe7429d746bce18fcf64a6abe449d3
sernadesigns <sernadesigns@gmail.com>
2020-01-23 00:45:35 -0800

    Updates ember-cli to Octane

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,14 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default Controller.extend();
+export default Controller.extend({
+	media: service(),
+
+	leftToBottom: computed('media', function() {
+		return this.media.isDesktop ? 'bottom' : 'left';
+	}),
+	rightToBottom: computed('media', function() {
+		return this.media.isDesktop ? 'bottom' : 'right';
+	})
+});

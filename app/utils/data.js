7a10cdf54099c0ef75fb2c4f03a39718af737585
sernadesigns <sernadesigns@gmail.com>
2020-02-28 00:09:03 -0800

    Adds data util to clean up application template

---
+++ app/utils/data.js
@@ -0,0 +1,49 @@
+const projects = [
+	{
+		name: 'Perryman Law Firm',
+		description: [
+			'A responsive website for the Perryman Law Firm in Temecula, CA.',
+			'Created in WordPress with a custom theme that I designed as a boilerplate for all of my web projects.'
+		],
+		url: 'http://perrymanlawfirm.com/',
+		repo: '',
+		thumbnail: {
+			image: 'assets/images/perryman-devices-1366x767.png',
+			alt: 'Perryman Law Firm website displayed on various devices'
+		}
+	},
+	{
+		name: 'Integrity Transportation',
+		description: [
+			'A responsive website for Integrity Transportation Service in Murrieta, CA.',
+			'Created in WordPress with a custom theme that I designed as a boilerplate for all of my web projects.'
+		],
+		url: '',
+		repo: '',
+		thumbnail: {
+			image: 'assets/images/integrity-devices-1366x767.png',
+			alt: 'Integrity Transportation website displayed on various devices'
+		}
+	}
+];
+
+const socials = [
+	{
+		name: 'twitter',
+		url: 'https://twitter.com/MichaelSerna22'
+	},
+	{
+		name: 'codepen',
+		url: 'https://codepen.io/sernadesigns/'
+	},
+	{
+		name: 'linkedin',
+		url: 'https://www.linkedin.com/in/michaelserna'
+	},
+	{
+		name: 'github',
+		url: 'https://github.com/sernadesigns'
+	}
+];
+
+export { projects, socials };

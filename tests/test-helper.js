e7bc0a3805c10bc83f2d54de5b33af025d24a7bb
sernadesigns <sernadesigns@gmail.com>
2020-09-20 20:33:12 -0600

    Upgrades to ember 3.20 and adds additional tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'serna-designs/app';
+import config from 'serna-designs/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
--- tests/unit/controllers/application-test.js
+++ tests/unit/controllers/application-test.js
@@ -4,9 +4,23 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Controller | application', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
-    let controller = this.owner.lookup('controller:application');
-    assert.ok(controller);
+  test('it computes leftToBottom', function(assert) {
+    const controller = this.owner.lookup('controller:application');
+		
+		controller.set('media.isDesktop', false);
+		assert.equal(controller.leftToBottom, 'left');
+
+		controller.set('media.isDesktop', true);
+		assert.equal(controller.leftToBottom, 'bottom');
+	});
+
+	test('it computes rightToBottom', function(assert) {
+    const controller = this.owner.lookup('controller:application');
+		
+		controller.set('media.isDesktop', false);
+		assert.equal(controller.rightToBottom, 'right');
+
+		controller.set('media.isDesktop', true);
+		assert.equal(controller.rightToBottom, 'bottom');
 	});
 });

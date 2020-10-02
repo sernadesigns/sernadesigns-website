3f612d4d387b28cef1b027a3308ef1bc4105fcbb
sernadesigns <sernadesigns@gmail.com>
2020-10-01 22:35:16 -0600

    Updated copyright text to computed property

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,8 @@
+import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import Controller from '@ember/controller';
-import { projects, socials } from '../utils/data';
+import { htmlSafe } from '@ember/template';
+import { projects, socials } from 'serna-designs/utils/data';
 
 export default class ApplicationController extends Controller {
 	@service
@@ -11,6 +12,11 @@ export default class ApplicationController extends Controller {
 		super(...arguments);
 		this.projects = projects;
 		this.socials = socials;
+	}
+
+	@computed
+	get copyrightText() {
+		return htmlSafe(`&copy; 2011-${new Date().getFullYear()} SernaDesigns Web Development`);
 	}
 
 	@computed('media.isDesktop')
